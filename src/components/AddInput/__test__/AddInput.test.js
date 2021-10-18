e13591f141c01597abb8450895ce9dc1d4be69cf
ergunsahe <66415270+ergunsahe@users.noreply.github.com>
2021-10-18 12:42:30 +0200

    fire click event and get input value empty string

--- src/components/AddInput/__test__/AddInput.test.js
+++ src/components/AddInput/__test__/AddInput.test.js
@@ -14,6 +14,7 @@ describe("AddInput", () =>{
     const inputElement = screen.getByPlaceholderText(/Add a new task here.../i);
     expect(inputElement).toBeInTheDocument();
   });
+
   it('should be able to type in input element', () => {
     render(
         <AddInput 
@@ -24,5 +25,19 @@ describe("AddInput", () =>{
     const inputElement = screen.getByPlaceholderText(/Add a new task here.../i);
     fireEvent.change(inputElement, {target:{value:"Go Grocery Shopping"}})
     expect(inputElement.value).toBe("Go Grocery Shopping");
+  });
+
+  it('should have empty input when add button is clicked', () => {
+    render(
+        <AddInput 
+            todos={[]}
+            setTodos={mockedSetTodo}
+        />
+    );
+    const inputElement = screen.getByPlaceholderText(/Add a new task here.../i);
+    const buttonElement = screen.getByRole("button", {name:/Add/i})
+    fireEvent.change(inputElement, {target:{value:"Go Grocery Shopping"}})
+    fireEvent.click(buttonElement,)
+    expect(inputElement.value).toBe("");
   });
 })
